0a55a35d6d679872fa4179ce5bd78f8b6d0bf484
miura_daisuke <miura_daisuke@unisight.co.jp>
2024-01-26 17:41:16 +0900

    fix: sw内のconfigを修正

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,16 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');importScripts('../config/config.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
 // Firebase の初期化
-const firebaseConfig = getFirebaseConfig();
+const firebaseConfig = {
+  apiKey: 'AIzaSyANJY8HmS40TKWQCkgzmortthrBB_2dqlE',
+  authDomain: 'jarinko-member-auth.firebaseapp.com',
+  projectId: 'jarinko-member-auth',
+  storageBucket: 'jarinko-member-auth.appspot.com',
+  messagingSenderId: '713584462327',
+  appId: '1:713584462327:web:a396df5b739d55cb42c6f2',
+  measurementId: 'G-DGTNN8PNF3',
+};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
