ae77323d8941a391f4d86458eb37c392e43c22f7
miura_daisuke <miura_daisuke@unisight.co.jp>
2024-02-01 16:29:18 +0900

    mod: チャットスケルトン修正

--- src/app/components/ui/SkeletonChat.tsx
+++ src/app/components/ui/SkeletonChat.tsx
@@ -10,37 +10,13 @@ export default function SkeletonChat() {
         <Skeleton w="6rem" h="0.5rem"/>
       </DateDivider>
       <OutgoingMessage message="" time="" received={false}>
+        <SkeletonText startColor="green.100" endColor="green.500" skeletonHeight={2} noOfLines={1} w="8rem"/>
+      </OutgoingMessage>
+      <OutgoingMessage message="" time="" received={false}>
         <SkeletonText startColor="green.100" endColor="green.500" skeletonHeight={2} noOfLines={1} w="5rem"/>
       </OutgoingMessage>
       <IncomingMessage message="" time="">
-        <SkeletonText startColor="orange.100" endColor="orange.500" skeletonHeight={2} noOfLines={3} w="10rem"/>
-      </IncomingMessage>
-      <OutgoingMessage message="" time="" received={false}>
-        <SkeletonText startColor="green.100" endColor="green.500" skeletonHeight={2} noOfLines={1} w="9rem"/>
-      </OutgoingMessage>
-      <OutgoingMessage message="" time="" received={false}>
-        <SkeletonText startColor="green.100" endColor="green.500" skeletonHeight={2} noOfLines={1} w="6rem"/>
-      </OutgoingMessage>
-      <IncomingMessage message="" time="">
-        <SkeletonText startColor="orange.100" endColor="orange.500" skeletonHeight={2} noOfLines={1} w="6rem"/>
-      </IncomingMessage>
-      <IncomingMessage message="" time="">
-        <SkeletonText startColor="orange.100" endColor="orange.500" skeletonHeight={2} noOfLines={1} w="4rem"/>
-      </IncomingMessage>
-      <OutgoingMessage message="" time="" received={false}>
-        <SkeletonText startColor="green.100" endColor="green.500" skeletonHeight={2} noOfLines={2} w="10rem"/>
-      </OutgoingMessage>
-      <DateDivider ms={0}>
-        <Skeleton w="6rem" h="0.5rem"/>
-      </DateDivider>
-      <OutgoingMessage message="" time="" received={false}>
-        <SkeletonText startColor="green.100" endColor="green.500" skeletonHeight={2} noOfLines={1} w="9rem"/>
-      </OutgoingMessage>
-      <OutgoingMessage message="" time="" received={false}>
-        <SkeletonText startColor="green.100" endColor="green.800" skeletonHeight={2} noOfLines={1} w="6rem"/>
-      </OutgoingMessage>
-      <IncomingMessage message="" time="">
-        <SkeletonText startColor="orange.100" endColor="orange.800" skeletonHeight={2} noOfLines={1} w="6rem"/>
+        <SkeletonText startColor="orange.100" endColor="orange.500" skeletonHeight={2} noOfLines={1} w="10rem"/>
       </IncomingMessage>
     </>
   );
